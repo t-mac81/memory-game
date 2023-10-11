0f08c42879332407c466f9c812c6b41d4ca91c57
Trevor MacMurray <macmurrt@gmail.com>
2023-10-11 15:56:01 -0700

    added main functionality

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-const gameContainer = document.getElementById("game");
-
+const gameContainer = document.getElementById('game');
+const startButton = document.querySelector('#start');
+const stopButton = document.querySelector('#stop');
+const hiddenItems = document.querySelectorAll('.hidden');
+const displayMoves = document.querySelector('#moves');
+const displayBestScore = document.querySelector('#best-score');
+const winText = document.querySelector('#win');
+const cards = gameContainer.children;
+let clickCount = 0;
+let moves = 0;
+let gameStatus = 0;
+const cardArr = [];
 const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
+  'red',
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'yellow',
+  'red',
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'yellow',
 ];
+
+// Start game button functionality
+startButton.addEventListener('click', handleStart);
+
+function handleStart() {
+  for (item of hiddenItems) {
+    item.classList.toggle('hidden');
+  }
+  startButton.classList.toggle('hidden');
+  startButton.removeEventListener('click', handleStart);
+  startButton.style.cursor = 'default';
+  stopButton.style.cursor = 'pointer';
+}
+
+// end game button functionality
+stopButton.addEventListener('click', handleStop);
+function handleStop() {
+  location.reload();
+}
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
@@ -44,24 +75,80 @@ let shuffledColors = shuffle(COLORS);
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
     // create a new div
-    const newDiv = document.createElement("div");
+    const newDiv = document.createElement('div');
 
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
 
     // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
+    newDiv.addEventListener('click', handleCardClick);
 
     // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
 
-// TODO: Implement this function!
+// handle card clicks
 function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  const color = event.target.className;
+  clickCount++;
+  cardArr.push(color);
+  const card = event.target;
+
+  // ensure the same color can't be clicked twice
+  card.removeEventListener('click', handleCardClick);
+
+  // manage each 'move' (2 clicks)
+  if (clickCount === 2) {
+    removeEvents();
+    setTimeout(addEvents, 1500);
+    setTimeout(hideCards, 1400);
+    if (cardArr[0] === cardArr[1]) {
+      addMatch(cardArr[0]);
+      gameStatus++;
+    }
+    moves++;
+    clickCount = 0;
+    cardArr.splice(0, 2);
+    displayMoves.textContent = `Moves: ${moves}`;
+
+    // handle game status
+    if (gameStatus === 6) {
+      winText.style.opacity = 1;
+      stopButton.textContent = 'Play Again';
+    }
+  }
+  event.target.style.backgroundColor = color;
 }
 
-// when the DOM loads
+// remove event listeners from all cards
+function removeEvents() {
+  for (let card of cards) {
+    card.removeEventListener('click', handleCardClick);
+  }
+}
+
+// add event listeners to all cards
+function addEvents() {
+  for (let card of cards) {
+    card.addEventListener('click', handleCardClick);
+  }
+}
+
+// add a 'match' className to the two matched cards
+function addMatch(color) {
+  const matchedCards = document.querySelectorAll(`.${color}`);
+  for (let card of matchedCards) {
+    card.classList.add('match');
+  }
+}
+
+// hide cards that were not matched
+function hideCards() {
+  for (let card of cards) {
+    if (!card.classList.contains('match')) card.removeAttribute('style');
+  }
+}
+
+// when the DOM loads shuffle colors
 createDivsForColors(shuffledColors);
